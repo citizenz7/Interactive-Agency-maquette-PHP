3c8f604c5ee182f423d938e567801c13ed10a690
citizenz7 <citizenz7@protonmail.com>
2020-05-19 10:00:53 +0200

    first commit

---
+++ main.php
@@ -0,0 +1,135 @@
+  <!-- Best design/ Best code section -->
+  <div class="container">
+    <div class="row">
+      <div id="bestdesign" class="col-sm-12 col-md-6 m-0 p-0 w-100 h-75">
+        <div class="paddingbestdesign text-light">
+          <p class="title1">Best<b>Design</b></p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+          <a href="#" class="myButton">Ask for price</a>
+        </div>
+      </div>
+      <div id="bestcode" class="col-sm-12 col-md-6 m-0 p-0 w-100 h-75">
+        <div class="paddingbestcode text-light">
+          <p class="title1">Best<b>Code</b></p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+          <a href="#" class="myButton">Ask for price</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- What we do section -->
+  <div class="container">
+
+    <div class="row">
+      <div id="whatwedo" class="col-sm-12">
+        <p class="lead-text1 text-center pt-5 pb-4">What we do?</p>
+      </div>
+      <div id="whatwedo" class="col-sm-12 col-md-6 m-0 p-0 w-100 h-75">
+        <div class="paddingwhatwedoleft">
+          <i class="fas fa-file-signature fa-3x float-left m-3 text-muted"></i>
+          <p class="lead-text2">Web design</p>
+          <p class="text-small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div id="whatwedo" class="col-sm-12 col-md-6 m-0 p-0 w-100 h-75">
+        <div class="paddingwhatwedoright">
+          <i class="fas fa-laptop fa-3x float-left m-3 text-muted"></i>
+          <p class="lead-text2">Web applications</p>
+          <p class="text-small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div id="whatwedo" class="col-sm-12 col-md-6 m-0 p-0 w-100 h-75">
+        <div class="paddingwhatwedoleft">
+          <i class="fas fa-file-image fa-3x float-left m-3 text-muted"></i>
+          <p class="lead-text2">Digital painting</p>
+          <p class="text-small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div id="whatwedo" class="col-sm-12 col-md-6 m-0 p-0 w-100 h-75">
+        <div class="paddingwhatwedoright">
+          <i class="fas fa-desktop fa-3x float-left m-3 text-muted"></i>
+          <p class="lead-text2">Desktop applications</p>
+          <p class="text-small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div id="whatwedo" class="col-sm-12 pt-1 pb-5 w-100 h-75 text-center">
+        <a href="#" class="myButton">Ask for price</a>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Porfolio section -->
+  <div class="container">
+    <div class="row">
+      <div class="col m-0 p-0">
+        <img class="img-fluid" src="img/portfolio/Layer12.jpeg" alt="portfolio1">
+      </div>
+      <div id="portfolio-darken" class="col m-0 p-0">
+        <p class="text-white text-center lead pt-5 pb-1">Netptune Template</p>
+        <p class="text-small text-white text-center pb-1">Alliquam sagittis neque in lectus semper, nec elementus<br> aecuscelerisque. Curabitur ullamcorper auctor mauris,<br>placera fermentum.</p>
+        <p class="text-center"><a href="#" class="myButton">Ask for price</a></p>
+      </div>
+      <div class="col m-0 p-0">
+        <img class="img-fluid" src="img/portfolio/Layer13.jpeg" alt="portfolio1">
+      </div>
+    </div>
+  </div>
+
+  <!-- About us -->
+  <div class="container">
+    <div class="row">
+      <div id="aboutus" class="col-sm-12 p-0 w-100 h-75">
+        <p class="text-white lead-text1 text-center pt-5 pb-1">About us!</p>
+        <p class="text-small text-muted text-center pb-5">Alliquam sagittis neque in lectus semper, nec elementus aecuscelerisque. Curabitur ullam-<br>corper auctor mauris, placera fermentum.</p>
+        <div class="row text-center photos">
+          <div class="col">
+            <img class="img-fluid rounded-circle" src="img/About/Layer16.jpeg" alt="John Doe"><br>
+            <span class="text-white">John Doe</span>
+            <p class="text-small text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+          <div class="col">
+            <img class="img-fluid rounded-circle" src="img/About/Layer17.jpeg" alt="John Doe"><br>
+            <span class="text-white">John Doe</span>
+            <p class="text-small text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+          <div class="col">
+            <img class="img-fluid rounded-circle" src="img/About/Layer18.jpeg" alt="John Doe"><br>
+            <span class="text-white">John Doe</span>
+            <p class="text-small text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+          <div class="col">
+            <img class="img-fluid rounded-circle" src="img/About/Layer19.jpeg" alt="John Doe"><br>
+            <span class="text-white">John Doe</span>
+            <p class="text-small text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-12 pt-1 pb-5 w-100 h-75 text-center mt-5 mb-4">
+          <a href="#" class="myButton">Watch more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Map -->
+  <div class="container">
+    <div class="row">
+      <div id="map" class="col-sm-12">
+        <p class="text-center">
+          <i id="location" class="fas fa-map-marker-alt fa-2x"></i>
+        </p>
+      </div>
+    </div>
+  </div>
